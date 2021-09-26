bad5e548f2a3656e8de00535145cadfba0a01586
guillemnobi <guillemguillem01@gmail.com>
2021-09-26 12:14:57 +0200

    chagens 2609

--- script.js
+++ script.js
@@ -1,41 +1,31 @@
-const element = React.createElement(
+const hello = React.createElement(
   'h1', // type
   {
     className: 'title',
-    onClick: () => {console.log('clicked');},
+    onClick: () => {alert('clicked');},
     style: { fontFamily: 'sans-serif', color: 'rgb(255, 100, 100)'}
   }, // props
   'Hello World!' // children
 );
+
+const goodbye = React.createElement(
+  'h2', // type
+  {
+    className: 'title2',
+    onClick: (e) => {console.log("clicked")},
+    style: { fontFamily: 'sans-serif', color: 'rgb(0, 0, 0)'}
+  }, // props
+  'GoodBye World!' // children
+);
+
 ReactDOM.render(
-  element,
+  hello,
   document.getElementById('root')
 );
 ReactDOM.render(
-  element,
+  goodbye,
   document.getElementById('root2')
 );
 
-// ...earlier code
 
-document.querySelectorAll('.button')
-  // use querySelectorAll to select all three spans with class "button"
-  .forEach(domNode => {
-    // use forEach to loop through each of the span DOM node
 
-    // retrieve the message ID from the data attribute through the dataset property
-    const messageId = parseInt(domNode.dataset.messageId);
-
-    // create and render a React element for a button and attach an onClick event handler, it will log the messageID above
-    const button = React.createElement(
-      'button',
-       {
-         onClick: () => { console.log(`liked message: ${messageId}`); }
-       },
-      'Like'
-     )
-     ReactDOM.render(
-       button,
-       domNode
-     )
-  });
